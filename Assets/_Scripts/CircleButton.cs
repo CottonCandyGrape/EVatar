1a00b20852c505b55676c5a31f99a56c08cc34e6
Jeongho16 <cup33c@naver.com>
2018-08-29 18:58:52 +0900

    CircleButton Update

--- Assets/_Scripts/CircleButton.cs
+++ Assets/_Scripts/CircleButton.cs
@@ -7,7 +7,7 @@ namespace EyeHelpers
 {
     public class CircleButton : MonoBehaviour
     {
-        GameObject video, keyboard, help;
+        GameObject video, keyboard, keyboardOnVideo, help, helpOnVideo;
 
         // Use this for initialization
         void Start()
@@ -18,6 +18,7 @@ namespace EyeHelpers
         // Update is called once per frame
         void Update()
         {
+
             ChangeCircleButton();
         }
 
@@ -25,7 +26,9 @@ namespace EyeHelpers
         {
             video = GameObject.Find("Video_Btn");
             keyboard = GameObject.Find("Keyboard_Btn");
+            keyboardOnVideo = GameObject.Find("Keyboard_Btn_OnVideo");
             help = GameObject.Find("Help_Btn");
+            helpOnVideo = GameObject.Find("Help_Btn_OnVideo");
         }
 
         private void ChangeCircleButton()
@@ -36,19 +39,41 @@ namespace EyeHelpers
             {//다른 모드에서 Video_Btn이 켜지지 않기 위함
                 video.SetActive(true);
                 keyboard.SetActive(false);
-                help.SetActive(false);                
+                keyboardOnVideo.SetActive(false);
+                help.SetActive(false);
+                helpOnVideo.SetActive(false);
             }
-            else if (ModeChangeManager.bKeyboard)
-            {
+            else if (ModeChangeManager.bKeyboard && ModeChangeManager.bHome)
+            {//Keyboard On, Video Off
                 video.SetActive(false);
                 keyboard.SetActive(true);
-                help.SetActive(false);                
+                keyboardOnVideo.SetActive(false);
+                help.SetActive(false);
+                helpOnVideo.SetActive(false);
             }
-            else if (ModeChangeManager.bHelp)
-            {
+            else if (ModeChangeManager.bKeyboard && !ModeChangeManager.bHome)
+            {//Keyboard On, Video On
                 video.SetActive(false);
                 keyboard.SetActive(false);
-                help.SetActive(true);                
+                keyboardOnVideo.SetActive(true);
+                help.SetActive(false);
+                helpOnVideo.SetActive(false);
+            }
+            else if (ModeChangeManager.bHelp && ModeChangeManager.bHome)
+            {//Help On, Video Off
+                video.SetActive(false);
+                keyboard.SetActive(false);
+                keyboardOnVideo.SetActive(false);
+                help.SetActive(true);
+                helpOnVideo.SetActive(false);
+            }
+            else if (ModeChangeManager.bHelp && !ModeChangeManager.bHome)
+            {//Help On, Video On
+                video.SetActive(false);
+                keyboard.SetActive(false);
+                keyboardOnVideo.SetActive(false);
+                help.SetActive(false);
+                helpOnVideo.SetActive(true);
             }
         }
     }
